94fb55985b64b1d839f97826c1b151618b60431f
baalajimaestro <baalajimaestro@pixelexperience.org>
2020-02-17 09:20:30 +0530

    Add selenium and unit test
    Signed-off-by: baalajimaestro <baalajimaestro@pixelexperience.org>

---
+++ selenium_runner.py
@@ -0,0 +1,47 @@
+#
+# Copyright © 2019 Maestro Creativescape
+#
+# SPDX-License-Identifier: GPL-3.0
+#
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver import ActionChains
+from time import sleep
+import re
+from os import remove
+from os import environ
+from dotenv import load_dotenv
+import sentry_sdk
+from sentry_sdk import capture_exception
+
+
+load_dotenv("config.env")
+chrome_options = Options()
+chrome_options.add_argument("--window-size=1920x1080")
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.implicitly_wait(30)
+
+# Grab the current window
+main_window_handle = None
+while not main_window_handle:
+    main_window_handle = driver.current_window_handle
+
+driver.get("http://127.0.0.1:8000")
+username=driver.find_element_by_id('exampleInputEmail1')
+pwd=driver.find_element_by_id('exampleInputPassword1')
+btn=driver.find_element_by_name('submit')
+username.send_keys('clemadr')
+pwd.send_keys('12345')
+btn.click()
+
+driver.get("http://127.0.0.1:8000/addfaculty")
+driver.get("http://127.0.0.1:8000/addtimetable")
+driver.get("http://127.0.0.1:8000/addexam")
+driver.get("http://127.0.0.1:8000/allotfaculty")
+btn = driver.find_element_by_name('submit')
+btn.click()
+sleep(10)
+driver.quit()
---
+++ tests/test1.py
@@ -0,0 +1,108 @@
+from django.test import TestCase
+from software.models import *
+from django.contrib.auth.models import User
+
+class DepartmentTestCase(TestCase):
+    def setUp(self):
+        Department.objects.create(did=1, dname="some_dept")
+        Department.objects.create(did=3, dname="unknown_dept")
+
+    def test_dept(self):
+        """Check if departments are identified"""
+        test1 = Department.objects.get(did=1)
+        test2 = Department.objects.get(did=3)
+        self.assertEqual(test1.dname, 'some_dept')
+        self.assertEqual(test2.dname, 'unknown_dept')
+
+class FDetailTestCase(TestCase):
+    def setUp(self):
+        lol=User(username="username", password="password")
+        lol.save()
+        Department.objects.create(did=1, dname="some_dept")
+        fdetail.objects.create(fid=lol, countofduties=5, timetable="ftimetable\\fac1 - Sheet1 (1)_4x4sSLy.csv", did=Department(did=1, dname="some_dept"))
+
+    def test_fdetail(self):
+        """Check if FDetails are identified"""
+        lol=User(username="username", password="password")
+        test1 = fdetail.objects.get(countofduties=5)
+        self.assertEqual(test1.countofduties, 5)
+
+class ExamTestCase(TestCase):
+    def setUp(self):
+        Exam.objects.create(eid=1, etimetable="ftimetable\\final-exam-tt - Sheet1 (2)_AY3kUlc.csv", year=2020, allot=False)
+
+    def test_exam(self):
+        """Check if Exam is identified"""
+        test1 = Exam.objects.get(eid=1)
+        self.assertEqual(test1.year, 2020)
+
+class SubjectTestCase(TestCase):
+    def setUp(self):
+        Subject.objects.create(sname="test_case", scode=1)
+
+    def test_subject(self):
+        """Subjects are correctly identified"""
+        test1 = Subject.objects.get(scode=1)
+        self.assertEqual(test1.sname, "test_case")
+
+class StrengthTestCase(TestCase):
+    def setUp(self):
+        Department.objects.create(did=1, dname="some_dept")
+        Strength.objects.create(department=Department(did=1, dname="some_dept"),year=2020, strength=60)
+
+    def test_strength(self):
+        """Strength is correctly identified"""
+        test1 = Strength.objects.get(year=2020)
+        self.assertEqual(test1.strength, 60)
+
+class BuildingTestCase(TestCase):
+    def setUp(self):
+        Department.objects.create(did=1, dname="some_dept")
+        Building.objects.create(department=Department(did=1, dname="some_dept"),buildingname="AB3")
+
+    def test_building(self):
+        """Buildings are correctly identified"""
+        test1 = Building.objects.get(buildingname="AB3")
+        self.assertEqual(test1.buildingname, "AB3")
+
+class RoomTestCase(TestCase):
+    def setUp(self):
+        Department.objects.create(did=1, dname="some_dept")
+        Building.objects.create(department=Department(did=1, dname="some_dept"),buildingname="AB3")
+        kek=Building(department=Department(did=1, dname="some_dept"),buildingname="AB3")
+        kek.save()
+        Rooms.objects.create(roomno="G303", building=kek)
+
+    def test_rooms(self):
+        """Rooms are correctly identified"""
+        test1 = Rooms.objects.get(roomno="G303")
+        self.assertEqual(test1.roomno,"G303")
+
+class AllotmentTestCase(TestCase):
+    def setUp(self):
+        lol=User(username="username", password="password")
+        lol.save()
+        Department.objects.create(did=1, dname="some_dept")
+        Building.objects.create(department=Department(did=1, dname="some_dept"),buildingname="AB3")
+        kek=Building(department=Department(did=1, dname="some_dept"),buildingname="AB3")
+        kek.save()
+        Rooms.objects.create(roomno="G303", building=kek)
+        lel=Rooms(roomno="G303", building=kek)
+        lel.save()         
+        allotment.objects.create(fid=lol, date="1/1/2020", time="9:30", ename="K0", semester="S7", sname="MOT", roomno=lel, alter=False)
+        
+    def test_allotment(self):
+        """Allotments are correctly identified"""
+        test1 = allotment.objects.get(ename="K0")
+        self.assertEqual(test1.semester, "S7")
+
+class AvailableTestCase(TestCase):
+    def setUp(self):
+        lol=User(username="username", password="password")
+        lol.save()
+        Available.objects.create(fid=lol, date="1/1/2020", time="9:30")
+
+    def test_available(self):
+        """Availables are correctly identified"""
+        test1 = Available.objects.get(date="1/1/2020")
+        self.assertEqual(test1.time,"9:30")
